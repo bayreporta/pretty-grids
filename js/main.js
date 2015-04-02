754771d00f4e0c72b954d44d8e80a8f121722113
bayreporta <bayreporta@gmail.com>
2015-04-02 12:24:59 -0700

    all good but mobile

--- js/main.js
+++ js/main.js
@@ -39,6 +39,10 @@ var prettyGrids = {
 		prettyGrids.populateData(prettyGrids.data, c, prettyGrids.output);
 	},
 	populateData: function(d, c){
+		for (var i = 0; i < c.colors.length; i++){
+			$('.p-legend:eq('+i+') div').css('background', c.colors[i]);
+		};
+
 		for (var i=0 ; i < c.totalCanvas; i++){
 			for (var ii=0; ii < c.totalY ; ii++){		
 				for (var iii = 0 ; iii < c.totalX ; iii++){
@@ -64,6 +68,12 @@ var prettyGrids = {
 			 prettyGrids.populateGrid(prettyGrids.calibrate);
 		});
 	},
+	popLegend: function(url){
+		newwindow=window.open(url,'p-legend','height=1000,width=400');
+		if (window.focus) {newwindow.focus()}
+		return false;
+	}
+
 }
 
 window.onload = function(){
